12ae0e617cdb6f985e2474153be9cfb68457017e
jchandra0 <jayachandrakoppula@gmail.com>
2024-07-15 10:14:57 +0530

    Add files via upload

---
+++ StaticVariable.java
@@ -0,0 +1,9 @@
+package one.java;
+
+public class StaticVariable {
+	public static String name=" Jaya Chandra";
+	public static void main(String[]args) {
+		System.out.println(StaticVariable.name);
+	}
+
+}
